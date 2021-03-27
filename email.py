7e8d5af385c840a64389c6598831f197eea1992d
crhodes-4 <72163484+crhodes-4@users.noreply.github.com>
2021-03-27 17:16:41 -0400

    Update email.py

--- email.py
+++ email.py
@@ -1,16 +1,30 @@
 from flask import Flask
-from flask_mail import Mail
+from flask_mail import Mail, Message
 
-app = Flask(__name__)
+app = Flask(name)
+
+app.config['MAIL_SERVER'] = 'smtp-mail.outlook.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'howitrate@hotmail.com'
+app.config['MAIL_PASSWORD'] = 'Orange!1'
+app.config['MAIL_DEFAULT_SENDER'] = 'howitrate@hotmail.com'
+app.config['MAIL_DEBUG'] = False
 
 
-if __name__ == '__main__':
-    app.config.update(
-        #MAIL_SERVER='smtp@gmail.com',
-        #MAIL_PORT=587,
-        MAIL_USE_SSL=True,
-        MAIL_USERNAME = 'howitrate@hotmail.com',
-        MAIL_PASSWORD = 'Orange!1'
-    )
+mail = Mail(app)
+@app.route('/')
 
-    mail = Mail(app)
+def index():
+
+    msg = Message("Hello, from HowItRate",
+                  sender= 'howitrate@hotmail.com',
+                  recipients=[])
+
+    msg.add_recipient("averdone3@gmail.com")
+    mail.send(msg)
+    return 'MESSAGE SENT'
+
+if name == 'main':
+    app.run(debug=False)
